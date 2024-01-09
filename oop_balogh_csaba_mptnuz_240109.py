360214ccfeb8c016b3c58b5d2aff50ae66d5c39e
csabibme <36829084+csabibme@users.noreply.github.com>
2024-01-09 18:22:10 +0100

    v1.1

--- oop_balogh_csaba_mptnuz_240109.py
+++ oop_balogh_csaba_mptnuz_240109.py
@@ -1,4 +1,5 @@
-# bicikli-kölcsönző rendszer
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
 
 class Bicikli:
     def __init__(self, tipus, ar, allapot):
@@ -6,14 +7,47 @@ class Bicikli:
         self.ar = ar
         self.allapot = allapot
 
+    @abstractmethod
+    def arkalkulacio (self):
+        pass
 
-class Kolcsonzo:
-    pass
+    def __str__(self):
+        return f"{self.tipus}, ára egy napra: {self.ar}"
+
+
+class Hegyi(Bicikli):
+    def arkalkulacio(self):
+        return self.ar
+
+
+class Orszaguti(Bicikli):
+    def arkalkulacio(self):
+        return self.ar * 1.15
+
+
+class Gyermek(Bicikli):
+    def arkalkulacio(self):
+        return self.ar * 0.80
 
 
 class Kolcsonzes:
     pass
 
-hegyi = Bicikli("hegyi kerékpár", 1500, "normál")
-orszaguti = Bicikli("orszaguti kerékpár", 1800, "normál")
-gyermek = Bicikli("gyermek kerékpár", 1000, "normál")
+
+class Kolcsonzo:
+    def __init__(self, nev):
+        self.nev = nev
+        self.kerekparok = []
+        self.esemeny = []
+
+    def __add__(self, other):
+        if isinstance(other, Bicikli):
+            self.kerekparok.append(other)
+        else:
+            print("Nem Kerékpár")
+
+    def keres(self, tipus):
+        for bicikli in self.kerekparok:
+            if bicikli.tipus == tipus and bicikli.allapot == "Elérhető":
+                return bicikli
+        return None
