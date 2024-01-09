9521044753b4342e88fb1c2046fd9a1fe9223870
csabibme <36829084+csabibme@users.noreply.github.com>
2024-01-09 18:40:49 +0100

    v1.4

--- oop_balogh_csaba_mptnuz_240109.py
+++ oop_balogh_csaba_mptnuz_240109.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
+from datetime import datetime
+
 
 class Bicikli:
     def __init__(self, tipus, ar, allapot):
@@ -8,7 +9,7 @@ class Bicikli:
         self.allapot = allapot
 
     @abstractmethod
-    def arkalkulacio (self):
+    def arkalkulacio(self):
         pass
 
     def __str__(self):
@@ -31,24 +32,69 @@ class Gyermek(Bicikli):
 
 
 class Kolcsonzes:
-    pass
+    def __init__(self, bicikli, datum):
+        self.bicikli = bicikli
+        self.datum = datum
+
+    def ar(self):
+        return self.bicikli.arkalkulacio()
+
+    def lemondas(self):
+        self.bicikli.allapot = "Elérhető"
+
+    def __str__(self):
+        return f"{self.bicikli.tipus} - {self.datum.strftime('%Y-%m-%d')} - {self.ar()} Ft"
 
 
 class Kolcsonzo:
     def __init__(self, nev):
         self.nev = nev
         self.kerekparok = []
-        self.esemeny = []
-
+        self.esemenyek = []
 
     def __add__(self, other):
         if isinstance(other, Bicikli):
             self.kerekparok.append(other)
         else:
-            print("Nem Kerékpár")
+            print("Ez nem Kerékpár")
 
     def keres(self, tipus):
         for bicikli in self.kerekparok:
             if bicikli.tipus == tipus and bicikli.allapot == "Elérhető":
                 return bicikli
         return None
+
+# ESEMÉNY metódusok
+
+    # Biciklik kölcsönzésének lehetősége egy adott napra, a kölcsönzés árának kiszámításával. (15 pont)
+    def kolcsonzes(self, bicikli, datum):
+        if bicikli.allapot == "Elérhető" and datum >= datetime.now():
+            bicikli.allapot = "Nem elréhető"
+            kolcsonzes = Kolcsonzes(bicikli, datum)
+            self.esemenyek.append(kolcsonzes)
+            return f"{bicikli.tipus} kölcsönzése megtörtént. Fizetendő: {kolcsonzes.ar()} Ft."
+        else:
+            return "Hibás dátum, vagy nem eléhető."
+
+    # Kölcsönzések lemondásának lehetősége. (5 pont)
+    def lemondas(self, kolcsonzes):
+        if kolcsonzes in self.esemenyek and kolcsonzes.datum >= datetime.now():
+            kolcsonzes.lemondas()
+            self.esemenyek.remove(kolcsonzes)
+            return f"{kolcsonzes.bicikli.tipus} lemondva."
+        else:
+            return "Nincs ilyen esemény."
+
+    # Kölcsönzések listázásának lehetősége. (5 pont)
+    def listazas(self):
+        if self.esemenyek:
+            for kolcsonzes in self.esemenyek:
+                print(kolcsonzes)
+        else:
+            print("Hiba.")
+
+
+# Készíts egy egyszerű felhasználói interfészt (20 pont)
+
+def interfesz():
+    pass
